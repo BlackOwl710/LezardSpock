4f31c9e267fa907fbfcd0c23673065a4c4b61f9e
Aynn <yann.maillot.29@gmail.com>
2017-11-30 17:15:07 +0100

    tour+score pas ok

--- pfls.js
+++ pfls.js
@@ -9,9 +9,34 @@ $(document).ready(function () {
             'margin-right' : '20%',
         });
         var setTurn = prompt('En combien de manche veux-tu jouer ? ');
+        $('#pcScore').css({
+            'position': 'absolute',
+            'display': 'flex',
+            'flex-direction' : 'row-reverse',
+            'top' : '55%',
+            'right': '80%',
+            'width': '20%',
+            'background-color': 'rgba(255, 255,255, 0.7)',
+            'border-radius' : '1%',
+        });
+        $('#playerScore').css({
+            'position': 'absolute',
+            'display': 'flex',
+            'flex-direction' : 'row-reverse',
+            'top' : '55%',
+            'left': '80%',
+            'width': '20%',
+            'background-color': 'rgba(255, 255,255, 0.7)',
+            'border-radius' : '1%',
+        });
     });
 
     var boardPcChoices = ["Stone", "Sheet", "Cisor", "Lezard", "Spock"];
+    var scorePlayer = 0;
+    var scorePc = 0;
+    var turn = 0;
+    var setTurn = 0;
+
     if(setTurn % 2 !== 0){
         $('.box').click(function () {
             var randPc = Math.floor(Math.random() * 5);
@@ -39,60 +64,85 @@ $(document).ready(function () {
             function checkStone() {
                 if (turnPc == 'Spock' || turnPc == 'Sheet') {
                     console.log('You Loose');
+                    scorePc += 1;
+                    turn +=1; 
                 }
                 else if (turnPc == 'Lezard' || turnPc == 'Cisor') {
                     console.log('You Win !');
+                    scorePlayer +=1;
+                    turn +=1;
                 }
                 else {
                     console.log('Equality');
+                    turn +=1;
                 }
             }
 
             function checkSheet() {
                 if (turnPc == 'Stone' || turnPc == 'Spock') {
                     console.log('You win !');
+                    scorePlayer +=1;
+                    turn +=1;
                 }
                 else if (turnPc == 'Cisor' || turnPc == 'Lezard') {
                     console.log('You Loose !');
+                    scorePc +=1;
+                    turn +=1;
                 }
                 else {
                     console.log('Equality');
+                    turn +=1;
                 }
             }
         
             function checkCisor() {
                 if (turnPc == 'Stone' || turnPc == 'Spock') {
                     console.log('You Loose !');
+                    scorePc +=1;
+                    turn +=1;
                 }
                 else if (turnPc == 'Sheet' || turnPlayer == 'Lezard') {
                     console.log('You Win !');
+                    scorePlayer +=1;
+                    turn +=1;
                 }
                 else {
                     console.log('Equality');
+                    turn +=1;
                 }
             }
         
             function checkLezard() {
                 if (turnPc == 'Stone' || turnPc == 'Cisor') {
                     console.log('You Loose');
+                    scorePc +=1;
+                    turn +=1;
                 }
                 else if (turnPc == 'Sheet' || turnPc == 'Spock') {
                     console.log('You Win !');
+                    scorePlayer +=1;
+                    turn +=1;
                 }
                 else {
                     console.log('Equality');
+                    turn +=1;
                 }
             }
         
             function checkSpock() {
                 if (turnPc == 'Stone' || turnPc == 'Cisor') {
                     console.log('You win !');
+                    scorePlayer +=1;
+                    turn +=1;
                 }
                 else if (turnPc == 'Sheet' || turnPc == 'Lezard') {
                     console.log('You Loose');
+                    scorePc +=1;
+                    turn +=1;
                 }
                 else {
                     console.log('Equality');
+                    turn +=1;
                 }
             }
             
@@ -115,5 +165,5 @@ $(document).ready(function () {
             displayPcChoice();
         });
     }
-    else{};
+    else{alert('YO !');};
 });
